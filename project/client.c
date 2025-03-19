8de0a37abfdda219ab39f8e6f784985df44cef69
saruM4N3 <zakobajo@gmail.com>
2025-03-20 00:12:46 +0100

    done some stuff

--- project/client.c
+++ project/client.c
@@ -6,15 +6,52 @@
 /*   By: zsonie <zsonie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 01:20:42 by zsonie            #+#    #+#             */
-/*   Updated: 2025/03/19 02:06:20 by zsonie           ###   ########.fr       */
+/*   Updated: 2025/03/19 21:52:09 by zsonie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+static void	send_message(int pid, char *message)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	while (message[i])
+	{
+		j = 0;
+		while (j < 8)
+		{
+			if (message[i] & (1 << j))
+				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
+			j++;
+			usleep(100);
+		}
+		i++;
+	}
+}
+static void end_of_transmission(int pid)
+{
+	int i = 0;
+	while (i < 8)
+	{
+		kill(pid, SIGUSR1);
+		usleep(100);
+		i++;
+	}
+}
 
 int main(int ac, char **av)
 {
 	if (ac != 3)
 		return (0);
-	return (0);
-
-	
+	message_lenght = strlen(av[2]) * 8;
+	send_message(atoi(av[1]), av[2]);
+	end_of_transmission(atoi(av[1]));
 }
